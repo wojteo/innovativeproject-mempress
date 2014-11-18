fd119aa95bb69ac45da146ee2016fb1ae714fea8
blisek <bartlomiej.lisek@hotmail.com>
2014-11-18 19:53:27 +0100

    Update DecisionTree.java

--- src/main/java/mempress/DecisionTree.java
+++ src/main/java/mempress/DecisionTree.java
@@ -3,6 +3,8 @@ package mempress;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Created by bartek on 2014-11-13.
@@ -18,40 +20,42 @@ public class DecisionTree<E> {
 
     public SmartListElement<E> processObject(E obj) {
         SmartListElement<E> tmp = null;
+        
+        ObjectDataCarrier metadata = new ObjectDataCarrier();
 
         for(DecisionTreeElement dte : processors) {
-            if(dte.checkConditions(obj, 0)) {
-                tmp = dte.processObject(obj);
+            if(dte.checkConditions(obj, metadata)) {
+                tmp = dte.processObject(obj, metadata);
                 if(tmp != null)
-                    break;
+                    return tmp;
             }
         }
 
-        return tmp;
+        throw new java.util.concurrent.CompletionException();
     }
 
     public interface DecisionTreeElement {
-        public boolean checkConditions(Object obj, long size);
-        public <E> SmartListElement<E> processObject(E obj);
+        public boolean checkConditions(Object obj, ObjectDataCarrier metadata);
+        public <E> SmartListElement<E> processObject(E obj, ObjectDataCarrier metadata);
     }
 
     private static class DecisionStoreIt implements DecisionTreeElement {
         @Override
-        public boolean checkConditions(Object obj, long size) {
+        public boolean checkConditions(Object obj, ObjectDataCarrier metadata) {
             boolean cond = true;
 
             return true;
         }
 
         @Override
-        public <E> SmartListElement<E> processObject(E obj) {
+        public <E> SmartListElement<E> processObject(E obj, ObjectDataCarrier metadata) {
             return new SimpleSmartListElement<E>(obj);
         }
     }
 
     private static class DecisionSerializeIt implements DecisionTreeElement {
         @Override
-        public boolean checkConditions(Object obj, long size) {
+        public boolean checkConditions(Object obj, ObjectDataCarrier metadata) {
             boolean cond = true;
             cond = cond && obj instanceof Serializable;
 
@@ -59,11 +63,17 @@ public class DecisionTree<E> {
         }
 
         @Override
-        public <E> SmartListElement<E> processObject(E obj) {
+        public <E> SmartListElement<E> processObject(E obj, ObjectDataCarrier metadata) {
             int hash = obj.hashCode();
             byte[] s = Serializer.ser(obj);
             Class<E> cl = (Class<E>)obj.getClass();
             return new ByteArraySmartListElement<>(hash, s, cl);
         }
     }
+}
+
+class ObjectDataCarrier
+{
+  public final Map<String, Object> data =
+    new HashMap<String, Object>();
 }
