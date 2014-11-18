a226b2211a34fa0c02cb1621af6b9ce2f4e90531
wojteo <grelawojciech@gmail.com>
2014-11-18 22:51:43 +0100

    MempressException

---
+++ src/main/java/mempress/MempressException.java
@@ -0,0 +1,15 @@
+package mempress;
+
+public class MempressException extends RuntimeException {
+
+	private static final long serialVersionUID = 6560510941064291204L;
+	
+	public MempressException() {}
+
+    //Constructor that accepts a message
+    public MempressException(String message)
+    {
+       super(message);
+    }
+	
+}
