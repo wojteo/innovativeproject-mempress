fc489531a2430dc3e10a219cb0bdd64212ad8513
wojteo <grelawojciech@gmail.com>
2014-12-04 16:54:21 +0100

    EfficiencyTest

--- src/main/java/effitest/test2.java
+++ src/main/java/effitest/test2.java
@@ -1,26 +1,47 @@
 package effitest;
+import java.io.Console;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
+
+import com.google.common.base.Stopwatch;
 
 public class test2 {
 
 	public static void main(String[] args){
-		ArrayList<wrap> sl = new ArrayList<wrap>();
+		System.out.println("Test wymaga pliku sample1.txt");
+		ArrayList<byte[]> sl = new ArrayList<byte[]>();
 		int i=0;
+		Console co = System.console();
+		co.readLine();
+		Stopwatch sw;
+		long timeAdd=0;
+		int countAdd=0;
+		
 		try{
-			for(i=0; i<1000000; i++){
-				sl.add(new wrap(43));
-				sl.add(new wrap(64));
-				sl.add(new wrap(24));
-				sl.add(new wrap(54));
-				sl.add(new wrap(23));
-				System.out.println(sl.size());
+			for(i=0; i<30; i++){
+				File f = new File("sample1.txt");
+				InputStream fis = new FileInputStream(f);
+				byte[] b = com.google.common.io.ByteStreams.toByteArray(fis);
+				
+				sw = Stopwatch.createStarted();
+				sl.add(b);
+				sw.stop();
+				timeAdd += sw.elapsed(java.util.concurrent.TimeUnit.NANOSECONDS);
+				countAdd++;
+				System.out.println("ArrayList.size() = "+sl.size());
 			}
 		}catch(OutOfMemoryError | Exception e){
-			System.out.println("Final is " + i + " iterations, with SmartList size = " + sl.size());
 			e.printStackTrace();
+			sl.clear();
+		}finally{
+			System.out.println("Final is " + i + " additions, with ArrayList size = " + sl.size());
+			System.out.println("\n"+ countAdd + " Add's with avg time = " + test.nanoTime(timeAdd/countAdd));
+			
 		}
-		
-		
-		
+			
 	}
+		
 }
+
