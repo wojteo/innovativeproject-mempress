dea4b4fb7742858fcde536def16c7d853d934656
wojteo <grelawojciech@gmail.com>
2014-12-18 16:25:27 +0100

    Renamed test2, deleted test

--- src/main/java/effitest/testBasics.java
+++ src/main/java/effitest/testBasics.java
@@ -1,6 +1,5 @@
 package effitest;
 
-import java.util.List;
 import java.io.Console;
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,9 +10,15 @@ import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-import mempress.*;
+import mempress.DecisionSerializeFile;
+import mempress.DecisionTree;
+import mempress.DecisionTreeBuilder;
+import mempress.Immutable;
+import mempress.MempressException;
+import mempress.SmartListBuilder;
 
 import com.google.common.base.Stopwatch;
 
@@ -30,7 +35,7 @@ class bytewrap implements Serializable, Immutable{
 	
 }
 
-public class test2{
+public class testBasics{
 	/**
 	 * Funkcja konwertująca nanosekundy na sekundy
 	 */
@@ -45,14 +50,22 @@ public class test2{
 	}
 	
 	public static void main(String[] args){
+		//int slip=3600;
+		int i;
+		for(i=1;;i++){
+			File f = new File("dane"+i+".csv");
+			if(!(f.exists() && !f.isDirectory())){
+				break;
+			}
+		}
 		
-		File csv = new File("dane.csv");
+		File csv = new File("dane"+i+".csv");
 		FileWriter writer=null;
 		int tick=0;
 		try{
 			csv.createNewFile();
 			writer = new FileWriter(csv);
-			writer.append("tick,add,get,remove\n").flush();
+			
 		}catch(IOException e){
 			e.printStackTrace();
 		}
@@ -67,28 +80,67 @@ public class test2{
 		long seed1=666;
 		long weightLimit = 1000;
 		int liczba_plikow = 800;
+		boolean dontUseFile=true;
 		
 		//stats
 		int countAdd=0, countRem=0, countGet=0;
 		long timeAdd=0, timeRem=0, timeGet=0;
 		Console co = System.console();
 		String type = "";
+		
+		System.out.println("Do you want to use configuration file?\n"
+				+ "'n' for no, <filename> for yes");
+		type=co.readLine();
+		String[] confData=null;
+		if(!type.equals("n")){
+			try{
+				byte[] encoded = Files.readAllBytes(Paths.get(type));
+				confData = new String(encoded).split("\n");
+				if(confData.length==5)	dontUseFile=false;
+			}catch(Exception e){
+				System.out.println("Failed to load configuration file");
+			}
+		}
+		
 
 		System.out.println("What size of weightLimit?");
-		type = co.readLine();
+		if(dontUseFile){
+			type = co.readLine();
+		}else{
+			type=confData[0];
+		}
 		weightLimit = Long.parseLong(type);
 		
 		System.out.println("How many sample files?");
-		type = co.readLine();
+		if(dontUseFile){
+			type = co.readLine();
+		}else{
+			type=confData[1];
+		}
 		liczba_plikow = Integer.parseInt(type);
 		
 		System.out.println("How many adds, inserts, add/inserts, add/insert/removes (one number for all) ?");
-		type = co.readLine();
+		if(dontUseFile){
+			type = co.readLine();
+		}else{
+			type=confData[2];
+		}
 		igrQ = igQ = addQ = Integer.parseInt(type);
 		
-		
+		System.out.println("What seed for RNG?");
+		if(dontUseFile){
+			type = co.readLine();
+		}else{
+			type=confData[3];
+		}
+		seed1 = Long.parseLong(type);
+				
 		System.out.println("Write 1 for ArrayList or 2 for SmartList");
-		type = co.readLine();
+		if(dontUseFile){
+			type = co.readLine();
+		}else{
+			type=confData[4];
+		}
 		List<bytewrap> sl = null;
 		
 		
@@ -96,17 +148,38 @@ public class test2{
 			System.out.println("ArrayListSelected");
 			sl = new ArrayList<bytewrap>();
 		}else if(type.equals("2")){
+			/*
+			//Start-1-OnlyZipFileSerialization SmartList
 			System.out.println("SmartListSelected");
+			DecisionTree< bytewrap> d = DecisionTreeBuilder
+					.<bytewrap>create()
+					.addTreeElement(new DecisionSerializeFile<bytewrap>()) //change to DecisionSerializeFile for normal serialization
+					.build();
+			sl = SmartListBuilder.<bytewrap>create().weightLimit(weightLimit).decisionTree(d).build();
+			//End-1
+			*/
+			//Start-2-Normal SmartList
 			sl = SmartListBuilder.<bytewrap>create().weightLimit(weightLimit).build();
+			//End-2
 			
 		}else{
 			System.out.println("Wrong. Setting ArrayList as default");
 			sl = new ArrayList<bytewrap>();
 		}
 		
-		int i=0;
 		Random rnd = new Random(seed1);
 		try{
+			writer.append("weightLimit,filesQuantity,operationsQuantity,seed,List\n").flush();
+			writer.append(weightLimit+","+liczba_plikow+","+addQ+","+seed1+",");
+				if(type.equals("2")){
+					writer.append("Smart");
+				}else{
+					writer.append("Array");
+				}
+				writer.append("List\n\n").flush();
+			writer.append("tick,add,get,remove\n").flush();
+			
+				
 			System.out.println("Starting insertOnly");
 			for(i=0; i<addQ; i++){
 				int x = rnd.nextInt(liczba_plikow)+1;
@@ -144,6 +217,7 @@ public class test2{
 			System.out.println("Starting getOnly");
 			//co.readLine();
 			for(i=0; i<igQ; i++){
+				//if(i%500==0) Thread.sleep(slip);
 				int pos = sl.size();
 				if(pos>0)
 					pos = rnd.nextInt(sl.size());
@@ -151,6 +225,7 @@ public class test2{
 					pos=0;
 				System.out.print(pos+"<-");
 				//random get
+					@SuppressWarnings("unused")
 					bytewrap bw ;
 					Stopwatch sw = Stopwatch.createStarted();
 					bw = sl.get(pos);
@@ -169,6 +244,7 @@ public class test2{
 			System.out.println("Starting insert-get");
 			//co.readLine();
 			for(i=0; i<igQ; i++){
+				//if(i%500==0) Thread.sleep(slip);
 				int x = rnd.nextInt(liczba_plikow)+1;
 				int pos = sl.size();
 				if(pos>0)
@@ -201,6 +277,7 @@ public class test2{
 					System.out.println("list.add("+pos+", "+pth+"), list.size = "+sl.size());
 				}else{
 					//random get
+					@SuppressWarnings("unused")
 					bytewrap bw ;
 					Stopwatch sw = Stopwatch.createStarted();
 					bw = sl.get(pos);
@@ -217,7 +294,8 @@ public class test2{
 			
 			System.out.println("Starting Add-Get-Remove");
 			//co.readLine();
-			for(i=0; i<igQ; i++){
+			for(i=0; i<igrQ; i++){
+				//if(i%500==0) Thread.sleep(slip);
 				int x = rnd.nextInt(liczba_plikow)+1;
 				int pos = sl.size();
 				if(pos>0)
@@ -250,6 +328,7 @@ public class test2{
 					System.out.println("list.add("+pos+", "+pth+"), list.size = "+sl.size());
 				}else if(what==1 || what==2){
 					//random get
+					@SuppressWarnings("unused")
 					bytewrap bw ;
 					Stopwatch sw = Stopwatch.createStarted();
 					bw = sl.get(pos);
@@ -262,6 +341,7 @@ public class test2{
 					System.out.println("list.get("+pos+"), list.size = "+sl.size());
 				}else{
 					//random remove
+					@SuppressWarnings("unused")
 					bytewrap bw ;
 					Stopwatch sw = Stopwatch.createStarted();
 					bw = sl.remove(pos);
@@ -280,27 +360,32 @@ public class test2{
 			System.out.println("OOME :(");
 			e.printStackTrace();
 			System.gc();
+		}catch(MempressException e){
+			e.printStackTrace();
 		}catch(Exception e){
-			System.out.println("Something went wrong");
 			e.printStackTrace();
 		}finally{
 			System.out.println("\nTotal: "+ (countAdd+countGet+countRem) + " operations.");
 			System.out.println("\n"+ countAdd + " Adds with avg time = " + nanoTime(timeAdd/countAdd));
 			System.out.println("\n"+ countGet + " Gets with avg time = " + nanoTime(timeGet/countGet));
 			System.out.println("\n"+ countRem + " Removes with avg time = " + nanoTime(timeRem/countRem));
-			
+			System.out.println("\nDetailed measures saved in " + csv.getName()+" file");
 		}
+		
+		try{
+			writer.append("\n addsAvg,getAvg,remAvg\n"
+					+ nanoTime(timeAdd/countAdd)+","+nanoTime(timeGet/countGet)+","+nanoTime(timeRem/countRem)).flush();
+		}catch(Exception e){}
 		
 		System.out.println("Press enter for GC");
 		co.readLine();
 		System.gc();
 		System.out.println("Press enter to delete+GC");
 		co.readLine();
-		sl = null;
+		sl=null;
 		System.gc(); 
-		System.out.println("Finished\n Press enter again");
-		co.readLine();
+		System.out.println("Finished\n Press enter to exit");
 		
-		
+		System.exit(1);
 	}
 };
