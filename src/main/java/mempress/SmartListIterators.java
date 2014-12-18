3d54510f67486d2ba160e978756cec0f8cf2f1f6
blisek <bartlomiej.lisek@hotmail.com>
2014-12-18 14:50:16 +0100

    ReversedPreloadIterator

--- src/main/java/mempress/SmartListIterators.java
+++ src/main/java/mempress/SmartListIterators.java
@@ -24,8 +24,17 @@ public class SmartListIterators {
     }
 
 
-    public static <T> ListIterator<T> makeReverseIterator(SmartList<T> smartList) {
-        return null;
+    public static <T> Iterator<T> makeReversedPreloadIterator(SmartList<T> smartList) {
+
+        return new ReversedPreloadIterator<T>(smartList);
+    }
+
+    public static <T> Iterator<T> makeReversedPreloadIterator(SmartList<T> smartList, int startIndex) {
+        return new ReversedPreloadIterator<T>(smartList, startIndex);
+    }
+
+    public static <T> Iterator<T> makeReversedPreloadIterator(SmartList<T> smartList, int startIndex, int numOfObjectsToPrepareInAdvance) {
+        return new ReversedPreloadIterator<T>(smartList, startIndex, numOfObjectsToPrepareInAdvance, 1);
     }
 
 
@@ -52,7 +61,7 @@ public class SmartListIterators {
             index = startIndex - 1;
             prepareInAdv = prepareObjectsInAdvance;
             buffer = new LinkedList<>();
-            tasks = Executors.newSingleThreadExecutor();
+            tasks = Executors.newFixedThreadPool(Math.min(3, prepareObjectsInAdvance));
         }
 
         @Override
@@ -87,14 +96,72 @@ public class SmartListIterators {
 
                 final int pos = i;
 
-                FutureTask<T> ft = new FutureTask<T>(new Callable<T>() {
-                    @Override
-                    public T call() throws Exception {
-                        return smartList.get(pos);
-                    }
-                });
+                FutureTask<T> ft = new FutureTask<>(() -> smartList.get(pos));
 
-                tasks.execute(ft);
+                tasks.submit(ft);
+                buffer.addLast(ft);
+            }
+        }
+    }
+
+    static class ReversedPreloadIterator<T> implements Iterator<T> {
+        private final SmartList<T> smartList;
+        private int index = -1;
+        private int prepareInAdv;
+        private LinkedList<Future<T>> buffer;
+        protected ExecutorService tasks;
+
+        public ReversedPreloadIterator(SmartList<T> smartList) { this(smartList, 0, 2, 1); }
+
+        public ReversedPreloadIterator(SmartList<T> smartList, int startIndex) { this(smartList, startIndex, 2, 1); }
+
+
+        public ReversedPreloadIterator(SmartList<T> smartList, int startIndex, int prepareInAdv, int step) {
+            Preconditions.checkNotNull(smartList);
+            Preconditions.checkArgument(startIndex >= 0 && startIndex < smartList.size() && prepareInAdv >= 1);
+
+            this.smartList = smartList;
+            this.index = startIndex + 1;
+            this.prepareInAdv = prepareInAdv;
+            this.buffer = new LinkedList<>();
+            this.tasks = Executors.newFixedThreadPool(Math.min(3, prepareInAdv));
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index - 1 >= 0;
+        }
+
+        @Override
+        public T next() {
+            int tmp = index - 1;
+            if(tmp < 0)
+                throw new NoSuchElementException();
+
+            prepareNextElements(Math.max(prepareInAdv - buffer.size(), 0));
+
+            try {
+                T obj = buffer.removeFirst().get();
+                index = tmp;
+                return obj;
+            } catch (Exception e) {
+                throw new NoSuchElementException(e.getMessage());
+            }
+        }
+
+        private void prepareNextElements(final int num) {
+
+            int startIndex = index - 1;
+
+            for(int i = startIndex; i > startIndex - num; --i) {
+                if(i < 0)
+                    break;
+
+                final int pos = i;
+
+                FutureTask<T> ft = new FutureTask<>(() -> smartList.get(pos));
+
+                tasks.submit(ft);
                 buffer.addLast(ft);
             }
         }
