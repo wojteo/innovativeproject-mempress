960934d376e3c07b775fdba5ced1cfb340caf242
blisek <bartlomiej.lisek@hotmail.com>
2014-12-19 03:40:21 +0100

    Mutable objects

---
+++ src/main/java/mempress/MutableListElement.java
@@ -0,0 +1,63 @@
+package mempress;
+
+/**
+ * Created by Bartek on 2014-12-19.
+ */
+public class MutableListElement<E> extends ListElement<E> {
+    private volatile int sharedCount;
+
+    public MutableListElement(ClassData data, Class<E> objectType) {
+        super(data, objectType);
+    }
+
+    public void decrementUseCount() {
+        --sharedCount;
+        if(sharedCount < 0)
+            throw new IllegalStateException();
+    }
+
+    public void incrementUseCount() {
+        ++sharedCount;
+    }
+
+    @Override
+    public E get(boolean countIt) {
+        while (sharedCount != 0);
+        return super.get(countIt);
+    }
+
+    @Override
+    public ClassData getData() {
+        while (sharedCount != 0);
+        return super.getData();
+    }
+
+    @Override
+    public void assign(ListElement<E> source) {
+        while (sharedCount != 0);
+        super.assign(source);
+    }
+
+    public boolean isAssignPossible() {
+        return sharedCount == 0;
+    }
+
+    public E getShared() {
+        E obj = getObject();
+        setUseCount(getUseCount() + 1);
+        return obj;
+    }
+
+    @Override
+    public int compareTo(ListElement<E> o) {
+        int ret = 0;
+        try {
+            ret = Integer.compare(sharedCount, ((MutableListElement<E>)o).sharedCount);
+        } catch (ClassCastException ex) {}
+        if(ret == 0) {
+            ret = super.compareTo(o);
+        }
+
+        return ret;
+    }
+}
---
+++ src/main/java/mempress/SharedObject.java
@@ -0,0 +1,42 @@
+package mempress;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * Created by Bartek on 2014-12-19.
+ */
+public class SharedObject<E> {
+    private final MutableListElement<E> sharedObject;
+    private volatile boolean blocked = false;
+
+    public SharedObject(MutableListElement<E> sharedObject) {
+        Preconditions.checkNotNull(sharedObject);
+        this.sharedObject = sharedObject;
+        sharedObject.incrementUseCount();
+    }
+
+    public E get() throws IllegalAccessException {
+        if(!blocked)
+            return sharedObject.getShared();
+        else
+            throw new IllegalAccessException();
+    }
+
+    public void release() {
+        if(!blocked) {
+            blocked = true;
+            sharedObject.decrementUseCount();
+        }
+    }
+
+    public boolean isReleased() {
+        return blocked;
+    }
+
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        release();
+    }
+}
