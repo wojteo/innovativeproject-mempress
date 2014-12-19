72fdb7c3620043ccc58672a63198abe864f86079
blisek <bartlomiej.lisek@hotmail.com>
2014-12-19 07:32:22 +0100

    Step iterators

--- src/main/java/mempress/SmartListIterators.java
+++ src/main/java/mempress/SmartListIterators.java
@@ -23,6 +23,10 @@ public class SmartListIterators {
         return new PreloadIterator<T>(smartList, startIndex, numOfObjectsToPrepareInAdvance);
     }
 
+    public static <T> Iterator<T> makePreloadIterator(SmartList<T> smartList, int startIndex, int numOfObjectsToPrepareInAdvance, int step) {
+        return new PreloadIterator<T>(smartList, startIndex, numOfObjectsToPrepareInAdvance, step);
+    }
+
 
     public static <T> Iterator<T> makeReversedPreloadIterator(SmartList<T> smartList) {
 
@@ -37,51 +41,73 @@ public class SmartListIterators {
         return new ReversedPreloadIterator<T>(smartList, startIndex, numOfObjectsToPrepareInAdvance, 1);
     }
 
+    public static <T> Iterator<T> makeReversedPreloadIterator(SmartList<T> smartList, int startIndex, int numOfObjectsToPrepareInAdvance, int step) {
+        return new ReversedPreloadIterator<T>(smartList, startIndex, numOfObjectsToPrepareInAdvance, step);
+    }
+
+
+    public static <T> Iterator<T> makeStepIterator(SmartList<T> smartList) {
+        return new StepIterator<T>(smartList);
+    }
+
+    public static <T> Iterator<T> makeStepIterator(SmartList<T> smartList, int startIndex) {
+        return new StepIterator<T>(smartList, startIndex);
+    }
+
+    public static <T> Iterator<T> makeStepIterator(SmartList<T> smartList, int startIndex, int step) {
+        return new StepIterator<T>(smartList, startIndex, step);
+    }
+
 
     static class PreloadIterator<T> implements Iterator<T> {
         private final SmartList<T> smartList;
         private int index = -1;
         private int prepareInAdv;
-//        private LinkedList<Future<T>> buffer;
         private ArrayDeque<Future<T>> buffer;
+        private int step;
+        private boolean init = false;
         protected ExecutorService tasks;
 
         public PreloadIterator(SmartList<T> sl) {
-            this(sl, 0, 2);
+            this(sl, 0, 2, 1);
         }
 
         public PreloadIterator(SmartList<T> sl, int startIndex) {
-            this(sl, startIndex, 2);
+            this(sl, startIndex, 2, 1);
         }
 
         public PreloadIterator(SmartList<T> sl, int startIndex, int prepareObjectsInAdvance) {
+            this(sl, startIndex, prepareObjectsInAdvance, 1);
+        }
+
+        public PreloadIterator(SmartList<T> sl, int startIndex, int prepareObjectsInAdvance, int step) {
             Preconditions.checkNotNull(sl);
+            Preconditions.checkArgument(step > 0);
             smartList = sl;
 
             Preconditions.checkArgument(startIndex >= 0 && startIndex < smartList.size());
-            index = startIndex - 1;
+            index = startIndex - step;
             prepareInAdv = prepareObjectsInAdvance;
-//            buffer = new LinkedList<>();
             buffer = new ArrayDeque<>(prepareInAdv + 1);
             tasks = Executors.newFixedThreadPool(Math.min(3, prepareObjectsInAdvance));
-
+            this.step = step;
         }
 
         @Override
         public boolean hasNext() {
-            return index + 1 < smartList.size();
+            return index + step < smartList.size();
         }
 
         @Override
         public T next() {
-            int tmp = index + 1;
+            int tmp = index + step;
+
             if(tmp >= smartList.size())
                 throw new NoSuchElementException();
 
             prepareNextElements(Math.max(prepareInAdv - buffer.size(), 0));
 
             try {
-//                T obj = buffer.removeFirst().get();
                 T obj = buffer.poll().get();
                 index = tmp;
                 return obj;
@@ -92,9 +118,9 @@ public class SmartListIterators {
 
         private void prepareNextElements(final int num) {
 
-            int startIndex = index + 1;
+            int startIndex = index + step;
 
-            for(int i = startIndex; i < startIndex + num; ++i) {
+            for(int i = startIndex; i < startIndex + num*step; i += step) {
                 if(smartList.size() <= i)
                     break;
 
@@ -104,9 +130,6 @@ public class SmartListIterators {
 
 
                 tasks.submit(ft);
-//                new Thread(ft).start();
-//                buffer.addLast(ft);
-//                buffer.offer(ft);
                 buffer.add(ft);
             }
         }
@@ -116,6 +139,7 @@ public class SmartListIterators {
         private final SmartList<T> smartList;
         private int index = -1;
         private int prepareInAdv;
+        private int step;
         private LinkedList<Future<T>> buffer;
         protected ExecutorService tasks;
 
@@ -123,26 +147,32 @@ public class SmartListIterators {
 
         public ReversedPreloadIterator(SmartList<T> smartList, int startIndex) { this(smartList, startIndex, 2, 1); }
 
+        public ReversedPreloadIterator(SmartList<T> smartList, int startIndex, int prepareInAdv) {
+            this(smartList, startIndex, prepareInAdv, 1);
+        }
 
         public ReversedPreloadIterator(SmartList<T> smartList, int startIndex, int prepareInAdv, int step) {
             Preconditions.checkNotNull(smartList);
             Preconditions.checkArgument(startIndex >= 0 && startIndex < smartList.size() && prepareInAdv >= 1);
+            Preconditions.checkArgument(step > 0);
 
             this.smartList = smartList;
-            this.index = startIndex + 1;
+            this.index = startIndex + step;
             this.prepareInAdv = prepareInAdv;
             this.buffer = new LinkedList<>();
             this.tasks = Executors.newFixedThreadPool(Math.min(3, prepareInAdv));
+            this.step = step;
         }
 
         @Override
         public boolean hasNext() {
-            return index - 1 >= 0;
+            return index - step >= 0;
         }
 
         @Override
         public T next() {
-            int tmp = index - 1;
+            int tmp = index - step;
+
             if(tmp < 0)
                 throw new NoSuchElementException();
 
@@ -159,9 +189,9 @@ public class SmartListIterators {
 
         private void prepareNextElements(final int num) {
 
-            int startIndex = index - 1;
+            int startIndex = index - step;
 
-            for(int i = startIndex; i > startIndex - num; --i) {
+            for(int i = startIndex; i > startIndex - num * step; i -= step) {
                 if(i < 0)
                     break;
 
@@ -172,6 +202,42 @@ public class SmartListIterators {
                 tasks.submit(ft);
                 buffer.addLast(ft);
             }
+        }
+    }
+
+    static class StepIterator<T> implements Iterator<T> {
+        private final SmartList<T> list;
+        private int index;
+        private int step;
+
+        public StepIterator(SmartList<T> list) {
+            this(list, 0, 1);
+        }
+
+        public StepIterator(SmartList<T> list, int index) {
+            this(list, index, 1);
+        }
+
+        public StepIterator(SmartList<T> list, int index, int step) {
+            Preconditions.checkNotNull(list);
+            Preconditions.checkArgument(index >= 0 && step > 0);
+            this.list = list;
+            this.index = index - step;
+            this.step = step;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index + step < list.size();
+        }
+
+        @Override
+        public T next() {
+            int tmp = index + step;
+            if(tmp >= list.size())
+                throw new NoSuchElementException();
+            index = tmp;
+            return list.get(index);
         }
     }
 }
